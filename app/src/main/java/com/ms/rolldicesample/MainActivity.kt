1c60c13e904af58f51add5e8d544c5dcf7f2dc9b
shirish79382 <56336274+shirish79382@users.noreply.github.com>
2020-11-30 12:32:45 +0530

    simple dice roller app

--- app/src/main/java/com/ms/rolldicesample/MainActivity.kt
+++ app/src/main/java/com/ms/rolldicesample/MainActivity.kt
@@ -6,62 +6,39 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var imgDice: ImageView
-    private val diceImages = mutableListOf<Int>()
-    private lateinit var animation: Animation
+    var diceImage : ImageView? = null;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        imgDice = findViewById(R.id.imgDice)
-        val btnRoll = findViewById<Button>(R.id.btnRoll)
-        btnRoll.setOnClickListener {
-            // disable the button click
-            btnRoll.isEnabled = false
-            getRandomValue()
-        }
+ override fun  onCreate (savedInstanceState: Bundle?) {
+     super.onCreate(savedInstanceState)
+     setContentView(R.layout.activity_main)
 
-        // add dice images to list
-        diceImages.add(R.drawable.ic_dice_one)
-        diceImages.add(R.drawable.ic_dice_two)
-        diceImages.add(R.drawable.ic_dice_three)
-        diceImages.add(R.drawable.ic_dice_four)
-        diceImages.add(R.drawable.ic_dice_five)
-        diceImages.add(R.drawable.ic_dice_six)
+     val rollButton: Button = findViewById(R.id.btnRoll)
+     btnRoll.setOnClickListener { rollDice() }
 
-        // initialize the animation
-        animation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.shake_anim)
+     diceImage = findViewById(R.id.imgDice)
 
+ }
+private fun rollDice() {
+    val randomInt = Random() .nextInt(6) + 1
+    Toast.makeText(this, "DICE: $randomInt", Toast.LENGTH_SHORT).show()
+
+    val drawaleResource = when(randomInt) {
+        1 -> R.drawable.ic_dice_one
+        2 -> R.drawable.ic_dice_two
+        3 -> R.drawable.ic_dice_three
+        4 -> R.drawable.ic_dice_four
+        5 -> R.drawable.ic_dice_five
+        6 -> R.drawable.ic_dice_six
+        else -> R.drawable.ic_dice
     }
+    diceImage?.setImageResource(drawaleResource)
 
-    private fun getRandomValue() {
-        val random = Random().nextInt(6) // returns number between 0-5
 
-        // start the animation
-        imgDice.startAnimation(animation)
-
-        // now we will get when the animation starts and when it ends
-
-        animation.setAnimationListener(object : Animation.AnimationListener{
-            override fun onAnimationRepeat(p0: Animation?) {
-            }
-
-            override fun onAnimationEnd(p0: Animation?) {
-                // when animation ends we will set the dice image
-                imgDice.setImageResource(diceImages.elementAt(random)) // this will set the dice image randomly
-                // enable the button click
-                btnRoll.isEnabled = true
-            }
-
-            override fun onAnimationStart(p0: Animation?) {
-                // when animation starts we will set default dice image
-                imgDice.setImageResource(R.drawable.ic_dice)
-            }
-        })
-    }
+}
 }
